cc508f3327510e7657db7f6ebbe2c95be2fa96bf
GeorgKreuzmayr <g.kreuzmayr@gmail.com>
2020-09-11 11:57:04 +0200

    remove dotnet version on windows

--- Travis/deploy.ps1
+++ Travis/deploy.ps1
@@ -1,20 +1,8 @@
 param(
-[string]$tag = ""
+[string]$apiKey,
+[string]$source
 )
-New-Variable -Name "VERSION_PATTERN" -Value "^[0-9]+\.[0-9]+\.[0-9]"
 
-if ("$tag" -match $VERSION_PATTERN)
-{
-    Write-Output "Deploying with Travis Tag: $tag"
-    exit 0
-    dotnet restore
-    dotnet build -c Release
-    dotnet pack -c Release --output nupkgs -p:PackageVersion="$tag" -p:AssemblyVersion="$tag.0"
-    dotnet test -c Release
-}
-else
-{
-    Write-Output "$tag is an illegal tag"
-    Write-Output "Git Tag has to resemble a package version (e.g. 1.0.0)."
-    exit 1
-}
+dotnet nuget push ./ArchUnitNET/nupkgs/TngTech.ArchUnitNET.*.nupkg -k $apiKey -s $source
+dotnet nuget push ./ArchUnitNET.xUnit/nupkgs/TngTech.ArchUnitNET.xUnit.*.nupkg -k $apiKey -s $source
+dotnet nuget push ./ArchUnitNET.NUnit/nupkgs/TngTech.ArchUnitNET.NUnit.*.nupkg -k $apiKey -s $source
