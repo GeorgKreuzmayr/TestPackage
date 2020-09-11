350fb0cd50e9df8d4c56ca6a3d95660aa3ace09b
GeorgKreuzmayr <g.kreuzmayr@gmail.com>
2020-09-11 12:04:32 +0200

    test and deploy

--- Travis/deploy.ps1
+++ Travis/deploy.ps1
@@ -3,6 +3,14 @@ param(
 [string]$source
 )
 
-dotnet nuget push ./ArchUnitNET/nupkgs/TngTech.ArchUnitNET.*.nupkg -k $apiKey -s $source
-dotnet nuget push ./ArchUnitNET.xUnit/nupkgs/TngTech.ArchUnitNET.xUnit.*.nupkg -k $apiKey -s $source
-dotnet nuget push ./ArchUnitNET.NUnit/nupkgs/TngTech.ArchUnitNET.NUnit.*.nupkg -k $apiKey -s $source
+if ("" -eq "")
+{
+    Write-Output "Key: $apiKey | Source: $source"
+    dotnet nuget push ./TestPackage/nupkgs/Georg.TestPackage.*.nupkg -k $apiKey -s $source
+}
+else
+{
+    dotnet nuget push ./ArchUnitNET/nupkgs/TngTech.ArchUnitNET.*.nupkg -k $apiKey -s $source
+    dotnet nuget push ./ArchUnitNET.xUnit/nupkgs/TngTech.ArchUnitNET.xUnit.*.nupkg -k $apiKey -s $source
+    dotnet nuget push ./ArchUnitNET.NUnit/nupkgs/TngTech.ArchUnitNET.NUnit.*.nupkg -k $apiKey -s $source
+}
--- Travis/test_linux.sh
+++ Travis/test_linux.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 set -ev
-dotnet restore
-dotnet build -c Release
-dotnet pack -c Release --output nupkgs -p:PackageVersion="0.0.0" -p:AssemblyVersion="0.0.0.0"
-dotnet test -c Release
+dotnet test -c Release --framework netcoreapp2.2
